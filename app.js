bfb9e7a70e1673ea3f457d39f00c47b376fcc3f1
Daniel Lim <hbt@markhamboards.com>
2016-12-26 14:39:32 -0500

    update app.js
    update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 (function() {
 
     var AppComponent = ng.core.Component({
-    
-    })
+        selector: 'my-app',
+        template: '<h1>Hello World!</h1>'
+    }).Class({
+        constructor: function() { }
+    });
 
+    var AppModule = ng.core.NgModule({
+        import: []
+    }).Class({
+        constructor: function() { }
+    });
+    
 }
 )
 ();

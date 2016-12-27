97cbb1ab1f98b788b33c594d5fcb93f8fb4a7880
Daniel Lim <hbt@markhamboards.com>
2016-12-26 20:53:51 -0500

    Add  Inject

--- app/random-quote.component.js
+++ app/random-quote.component.js
@@ -1,1 +1,28 @@
-"use strict";
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function (app) {
+  var _dec, _class;
+
+  var Component = ng.core.Component;
+  var Inject = mg.core.Inject;
+  var QuoteService = app.QuoteService;
+
+  var RandomQuoteComponent = (_dec = Component({
+    selector: 'random-quote',
+    template: '<p><em>{{quote.line}}</em> - {{quote.author}}</p>'
+  }), _dec(_class = function RandomQuoteComponent(quoteService) {
+    var _this = this;
+
+    _classCallCheck(this, RandomQuoteComponent);
+
+    quoteService.generateRandomQuotes(2000, function (quote) {
+      return _this.quote = quote;
+    });
+  }) || _class);
+  Inject(QuoteService)(RandomQuoteComponent, null, 0);
+
+
+  app.RandomQuoteComponent = RandomQuoteComponent;
+})(window.app || (window.app = {}));
--- src/random-quote.component.js
+++ src/random-quote.component.js
@@ -1,34 +1,19 @@
 (function(app) {
-  var Component = ng.core.Component;
-  var QuoteService = app.QuoteService;
+  
+  let Component = ng.core.Component;
+  let Inject = mg.core.Inject;
+  let QuoteService = app.QuoteService;
 
-  
-  
   @Component({
     selector: 'random-quote',
     template: '<p><em>{{quote.line}}</em> - {{quote.author}}</p>'
   })
 
   class RandomQuoteComponent {
-      constructor: (QuoteService) {
+      constructor(@Inject(QuoteService) quoteService) {
         quoteService.generateRandomQuotes(2000, quote => this.quote = quote);      
       } 
   }
-
-  // ===
-
-  
-
-
-  app.RandomQuoteComponent = Component({
-    selector: 'random-quote',
-    template: '<p><em>{{quote.line}}</em> - {{quote.author}}</p>'
-  })
-  .Class({
-    constructor: [QuoteService, function RandomQuoteComponent(quoteService) {      
-      quoteService.generateRandomQuotes(2000, quote => this.quote = quote);      
-    }]
-  });
 
   app.RandomQuoteComponent = RandomQuoteComponent;
 
